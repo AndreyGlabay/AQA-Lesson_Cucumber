054e4181ff9ef940f484c85d02595374a04d6612
Andriy Glabay <rishelevsky@gmail.com>
2024-02-07 23:41:32 +0200

    LoginSteps: implements Steps Definitions for Successful manual login scenario.

--- src/test/java/scenarious/LoginSteps.java
+++ src/test/java/scenarious/LoginSteps.java
@@ -1,4 +1,34 @@
 package scenarious;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class LoginSteps {
+    @Given("the user on the login page")
+    public void loginPage(){
+        System.out.println("Background: Login page");
+    }
+
+    @When("Valid username")
+    public void validLogin(){
+        System.out.println("Scenario: Successful manual login / Step: Valid username");
+    }
+
+    @And("Valid password")
+    public void validPassword(){
+        System.out.println("Scenario: Successful manual login / Step: Valid password");
+    }
+
+    @And("Click Login button")
+    public void clickLoginButton(){
+        System.out.println("Scenario: Successful manual login / Step: click Login button");
+    }
+
+    @Then("Redirect to the home page")
+    public void redirectToHomePage(){
+        System.out.println("Scenario: Successful manual login / Step: redirect to the home page");
+    }
+
 }
