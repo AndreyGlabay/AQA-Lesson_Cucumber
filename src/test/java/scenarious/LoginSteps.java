069e1c519bcd066147e070db2640bb5023402e2b
Andriy Glabay <rishelevsky@gmail.com>
2024-02-08 18:17:02 +0200

    Step definitions and Feature: modify steps for avoid annotation duplication.

--- src/test/java/scenarious/LoginSteps.java
+++ src/test/java/scenarious/LoginSteps.java
@@ -21,24 +21,24 @@ public class LoginSteps {
 
 
     // Scenario: Successful manual login
-    @When("the user enters their username to the field <login>")
+    @When("the user enters their username to the field <login> during manual login")
     public void validLogin(){
-        System.out.println("Scenario: the user enters their username to the field <login>");
+        System.out.println("the user enters their username to the field <login> during manual login");
     }
 
-    @And("the user enters valid  password to the field <password>")
+    @And("the user enters valid  password to the field <password> during manual login")
     public void validPassword(){
-        System.out.println("Scenario: the user enters valid  password to the field <password>");
+        System.out.println("the user enters valid  password to the field <password> during manual login");
     }
 
-    @And("the user clicks the <login> button")
+    @And("the user clicks the <login> button after entering valid username and valid password")
     public void clickLoginButtonDuringManualLogin(){
-        System.out.println("the user clicks the <login> button");
+        System.out.println("the user clicks the <login> button after entering valid username and valid password");
     }
 
-    @Then("the user is redirected to the home page")
+    @Then("the user is redirected to the home page after manual login")
     public void redirectToHomePageAfterManualLogin(){
-        System.out.println("the user is redirected to the home page");
+        System.out.println("the user is redirected to the home page after manual login");
     }
 
 
@@ -49,8 +49,8 @@ public class LoginSteps {
     @And("in the window that appeared, the user clicked on their Google account")
     public void userClicksTheirGoogleAcc() {System.out.println("in the window that appeared, the user clicked on their Google account");}
 
-    @Then("the user is redirected to the home page")
-    public void userRedirectToHomePAgeAfterSso() {System.out.println("the user is redirected to the home page");}
+    @Then("the user is redirected to the home page after SSO")
+    public void userRedirectToHomePAgeAfterSso() {System.out.println("the user is redirected to the home page after SSO");}
 
 
     //  Scenario: Unsuccessful manual login - wrong username
@@ -60,11 +60,11 @@ public class LoginSteps {
     @And("the user enters valid  password to the field <password>")
     public void userEntersValidPasswordDuringSso() {System.out.println("the user enters valid  password to the field <password>");}
 
-    @And("the user clicks the <login> button")
-    public void userClicksLoginButtonDuringSsoVar1() {System.out.println("the user clicks the <login> button");}
+    @And("the user clicks the <login> button after entering invalid username and valid password")
+    public void userClicksLoginButtonDuringSsoVar1() {System.out.println("tthe user clicks the <login> button after entering invalid username and valid password");}
 
-    @Then("the error massage come: \"ERROR: Login or password is incorrect\"")
-    public void errorMessageAfterSsoWithWrongLogin() {System.out.println("the error massage come: \"ERROR: Login or password is incorrect\"");}
+    @Then("the error message for wrong username comes: \"ERROR: Login or password is incorrect\"")
+    public void errorMessageAfterSsoWithWrongLogin() {System.out.println("the error message for wrong username comes: \"ERROR: Login or password is incorrect\"");}
 
 
     //  Scenario: Unsuccessful manual login - wrong password
@@ -74,9 +74,9 @@ public class LoginSteps {
     @And("the user enters invalid  password to the field <password>")
     public void userEntersIncorrectPasswordDuringSso() {System.out.println("the user enters invalid  password to the field <password>");}
 
-    @And("the user clicks the <login> button")
-    public void userClicksLoginButtonDuringSsoVar2() {System.out.println("the user clicks the <login> button");}
+    @And("the user clicks the <login> button after entering valid username and invalid password")
+    public void userClicksLoginButtonDuringSsoVar2() {System.out.println("the user clicks the <login> button after entering valid username and invalid password");}
 
-    @Then("the error massage come: \"ERROR: Login or password is incorrect\"")
-    public void errorMessageAfterSsoWithWrongPassword() {System.out.println("the error massage come: \"ERROR: Login or password is incorrect\"");}
+    @Then("the error message for wrong password comes: \"ERROR: Login or password is incorrect\"")
+    public void errorMessageAfterSsoWithWrongPassword() {System.out.println("the error message for wrong password comes: \"ERROR: Login or password is incorrect\"");}
 }
--- src/test/resources/login.feature
+++ src/test/resources/login.feature
@@ -5,24 +5,24 @@ Feature: feature1
     Then showed fields <login>, <password> and <SSO with Google>
 
   Scenario: Successful manual login
-    When the user enters their username to the field <login>
-    And the user enters valid  password to the field <password>
-    And the user clicks the <login> button
-    Then the user is redirected to the home page
+    When the user enters their username to the field <login> during manual login
+    And the user enters valid  password to the field <password> during manual login
+    And the user clicks the <login> button after entering valid username and valid password
+    Then the user is redirected to the home page after manual login
 
   Scenario:  Successful SSO login
     When the user clicks SSO with Google <G> button
     And in the window that appeared, the user clicked on their Google account
-    Then the user is redirected to the home page
+    Then the user is redirected to the home page after SSO
 
   Scenario: Unsuccessful manual login - wrong username
     When the user enters incorrect username to the field <login>
     And the user enters valid  password to the field <password>
-    And the user clicks the <login> button
-    Then the error massage come: "ERROR: Login or password is incorrect"
+    And the user clicks the <login> button after entering invalid username and valid password
+    Then the error message for wrong username comes: "ERROR: Login or password is incorrect"
 
   Scenario: Unsuccessful manual login - wrong password
     When the user enters their username to the field <login>
     And the user enters invalid  password to the field <password>
-    And the user clicks the <login> button
-    Then the error massage come: "ERROR: Login or password is incorrect"
+    And the user clicks the <login> button after entering valid username and invalid password
+    Then the error message for wrong password comes: "ERROR: Login or password is incorrect"
