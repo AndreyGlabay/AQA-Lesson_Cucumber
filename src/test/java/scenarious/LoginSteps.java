bc60df205b1cda5c84bc0185e088f6f62aaeaa8d
Andriy Glabay <rishelevsky@gmail.com>
2024-02-08 17:14:21 +0200

    Step Definitions 'LoginSteps': add definitions

--- src/test/java/scenarious/LoginSteps.java
+++ src/test/java/scenarious/LoginSteps.java
@@ -6,29 +6,77 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class LoginSteps {
+
+    // Background
     @Given("the user on the login page")
-    public void loginPage(){
-        System.out.println("Background: Login page");
+    public void userOnLoginPage(){
+        System.out.println("Background: User on Login page");
     }
 
-    @When("Valid username")
+    @And("the user open login form")
+    public void userOpenLoginForm(){ System.out.println("he user open login form");}
+
+    @Then("showed fields <login>, <password> and <SSO with Google>")
+    public void showedFields(){System.out.println("showed fields <login>, <password> and <SSO with Google>");}
+
+
+    // Scenario: Successful manual login
+    @When("the user enters their username to the field <login>")
     public void validLogin(){
-        System.out.println("Scenario: Successful manual login / Step: Valid username");
+        System.out.println("Scenario: the user enters their username to the field <login>");
     }
 
-    @And("Valid password")
+    @And("the user enters valid  password to the field <password>")
     public void validPassword(){
-        System.out.println("Scenario: Successful manual login / Step: Valid password");
+        System.out.println("Scenario: the user enters valid  password to the field <password>");
     }
 
-    @And("Click Login button")
-    public void clickLoginButton(){
-        System.out.println("Scenario: Successful manual login / Step: click Login button");
+    @And("the user clicks the <login> button")
+    public void clickLoginButtonDuringManualLogin(){
+        System.out.println("the user clicks the <login> button");
     }
 
-    @Then("Redirect to the home page")
-    public void redirectToHomePage(){
-        System.out.println("Scenario: Successful manual login / Step: redirect to the home page");
+    @Then("the user is redirected to the home page")
+    public void redirectToHomePageAfterManualLogin(){
+        System.out.println("the user is redirected to the home page");
     }
 
+
+    // Scenario:  Successful SSO login
+    @When("the user clicks SSO with Google <G> button")
+    public void userClicksSsoWithGoogle() {System.out.println("the user clicks SSO with Google <G> button");}
+
+    @And("in the window that appeared, the user clicked on their Google account")
+    public void userClicksTheirGoogleAcc() {System.out.println("in the window that appeared, the user clicked on their Google account");}
+
+    @Then("the user is redirected to the home page")
+    public void userRedirectToHomePAgeAfterSso() {System.out.println("the user is redirected to the home page");}
+
+
+    //  Scenario: Unsuccessful manual login - wrong username
+    @When("the user enters incorrect username to the field <login>")
+    public void userEntersIncorrectUsernameDuringSso() {System.out.println("the user enters incorrect username to the field <login>");}
+
+    @And("the user enters valid  password to the field <password>")
+    public void userEntersValidPasswordDuringSso() {System.out.println("the user enters valid  password to the field <password>");}
+
+    @And("the user clicks the <login> button")
+    public void userClicksLoginButtonDuringSsoVar1() {System.out.println("the user clicks the <login> button");}
+
+    @Then("the error massage come: \"ERROR: Login or password is incorrect\"")
+    public void errorMessageAfterSsoWithWrongLogin() {System.out.println("the error massage come: \"ERROR: Login or password is incorrect\"");}
+
+
+    //  Scenario: Unsuccessful manual login - wrong password
+    @When("the user enters their username to the field <login>")
+    public void userEntersValidUsernameDuringSso() {System.out.println("the user enters their username to the field <login>");}
+
+    @And("the user enters invalid  password to the field <password>")
+    public void userEntersIncorrectPasswordDuringSso() {System.out.println("the user enters invalid  password to the field <password>");}
+
+    @And("the user clicks the <login> button")
+    public void userClicksLoginButtonDuringSsoVar2() {System.out.println("the user clicks the <login> button");}
+
+    @Then("the error massage come: \"ERROR: Login or password is incorrect\"")
+    public void errorMessageAfterSsoWithWrongPassword() {System.out.println("the error massage come: \"ERROR: Login or password is incorrect\"");}
 }
